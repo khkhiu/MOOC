93d790b6d8a582acf0178544ff915899dd591690
Khiu Kim Hong <khkhiu@gmail.com>
2023-12-25 16:03:44 +0800

    Added functions

--- Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
+++ Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
@@ -5,3 +5,53 @@ let currentWeapon = 0;
 let fighting;
 let monsterHealth;
 let inventory = ["stick"];
+
+//Use const to declare read only variables
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const button3 = document.querySelector("#button3");
+
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+// Init buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function goTown(){
+    
+}
+
+function goStore(){
+    button1.innerText = "Buy 10 health (cost 10 gold)";
+    button1.onclick = buyHealth;
+    text.innerText = "You enter the store.";
+
+    button2.innerText = "Buy weapon (cost 30 gold)";
+    button2.onclick = buyWeapon;
+    button3.innerText = "Go to town square";
+    button3.onclick = goTown;
+}
+
+function goCave(){
+    console.log("Going to cave.");
+}
+
+function fightDragon(){
+    console.log("Fighting dragon.");
+}
+
+function buyHealth(){
+
+}
+
+function buyWeapon(){
+
+}
+
