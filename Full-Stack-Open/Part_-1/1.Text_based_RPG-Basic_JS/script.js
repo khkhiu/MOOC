8c2b594219c56249d0f1b77a9f37110701d58d2b
khkhiu <khkhiu@gmail.com>
2023-12-30 20:54:30 +0800

    Update script.js

--- Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
+++ Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
@@ -102,7 +102,7 @@ function buyWeapon() {
     gold -= 30;
     currentWeapon++;
     goldText.innerText = gold;
-    let newWeapon = weapons;
+    let newWeapon = weapons[currentWeapon];
     text.innerText = "You now have a new weapon."
   }
 }
