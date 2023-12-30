88b1337714a762c1f641b406768436ed1f6254bd
khkhiu <khkhiu@gmail.com>
2023-12-30 20:29:48 +0800

    Update script.js

--- Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
+++ Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
@@ -19,6 +19,13 @@ const monsterStats = document.querySelector("#monsterStats");
 const monsterName = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
 
+const weapons = [
+  { name: 'stick', power: 5 },
+  { name: 'dagger', power: 30 },
+  { name: 'claw hammer', power: 50 },
+  { name: 'sword', power: 100 }
+];
+
 const locations = [
     {
         name: "town square",
@@ -85,6 +92,18 @@ function buyHealth() {
   health += 10;
   goldText.innerText = gold;
   healthText.innerText = health;
+  } else {
+    text.innerText = "You do not have enough gold to buy health."
+  }
+}
+
+function buyWeapon() {
+  if (gold >= 30) {
+    gold -= 30;
+    currentWeapon++;
+    goldText.innerText = gold;
+    let newWeapon = weapons;
+    text.innerText = "You now have a new weapon."
   }
 }
 
