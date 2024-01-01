e41cdcbe8afef7fcbc67d9bf14ea92c836dc36a1
Khiu Kim Hong <khkhiu@gmail.com>
2024-01-01 20:34:29 +0800

    Update script.js

--- Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
+++ Full-Stack-Open/Part_-1/1.Text_based_RPG-Basic_JS/script.js
@@ -98,12 +98,22 @@ function buyHealth() {
 }
 
 function buyWeapon() {
-  if (gold >= 30) {
-    gold -= 30;
-    currentWeapon++;
-    goldText.innerText = gold;
-    let newWeapon = weapons[currentWeapon];
-    text.innerText = "You now have a new weapon."
+  if (currentWeapon > 3){
+    if (gold >= 30) {
+      gold -= 30;
+      currentWeapon++;
+      goldText.innerText = gold;
+      //weapons[currentWeapon] is an object. Use dot notation to get the name property of that object.
+      let newWeapon = weapons[currentWeapon].name;
+      //Use concat operator to state weapon name
+      text.innerText = "You now have a " + newWeapon + ".";
+      //Push new newWeapon to end of inventory araay using push() method
+      inventory.push(newWeapon);
+      //add new text to the end of text.innerText
+      text.innerText += " In your inventory you have: " + inventory + ".";
+    } else {
+      text.innerText = "You do not have enough gold to buy a weapon."
+    }
   }
 }
 
