6b5508fde53b051034af529a1ebbf4c6958ea689
khkhiu <khkhiu@gmail.com>
2024-08-01 22:57:40 +0800

    Reorganised C# MOOC
    Removed redundant items in C# MOOC

--- Foundational_C#_with_Microsoft/Part_1/1-Write_your_first_Csharp_code/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿/*
-Write code that produces the following output:
-This is the first line.
-This is the second line.
-*/
-Console.WriteLine("This is the first line.");
-Console.WriteLine("This is the second line.");
