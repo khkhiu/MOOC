48639c904b05413b9e28eed9c28567fbefb532ce
khkhiu <khkhiu@gmail.com>
2024-08-10 17:49:27 +0800

    EX 1.5 complete

--- Full-Stack-Open/Part_1/src/App.jsx
+++ Full-Stack-Open/Part_1/src/App.jsx
@@ -17,6 +17,64 @@ Modify the variable definitions of the App component to use objects
 Also refactor the application so that it still works
 */
 
+/* EX 1.5
+Change the course and its parts into a single JavaScript object. Fix everything that breaks.
+*/
+
+const Header = (props) => {
+  console.log(props)
+  return(
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Content = (props) => {
+  console.log(props)
+  return(
+    <div>
+      <p>{props.course.parts[0].name}: {props.course.parts[0].exercises}</p>
+      <p>{props.course.parts[1].name}: {props.course.parts[1].exercises}</p>
+      <p>{props.course.parts[2].name}: {props.course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return(
+    <p>Number of exercises = {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}</p>
+  )
+
+}
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content course = {course}/>
+      <Total course = {course}/>
+    </div>
+  )
+}
+
 /* EX 1.4
 Place the objects into an array. 
 Modify the variable definitions of App into the following form and modify the other parts of the application accordingly.
@@ -24,7 +82,7 @@ However, do not pass different objects as separate props from the App component 
 Instead, pass them directly as an array.
 */
 
-
+/* EX 1.4
 const Header = (props) => {
   console.log(props)
   return(
@@ -76,7 +134,7 @@ const App = () => {
     </div>
   )
 }
-
+*/
 
 /* Code for EX1.3
 const App = () => {
