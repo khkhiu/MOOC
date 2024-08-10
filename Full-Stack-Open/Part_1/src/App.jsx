2ebae8e72166e228be860b554abe4c9981ec9ae7
khkhiu <khkhiu@gmail.com>
2024-08-10 12:08:59 +0800

    Renamed folder

---
+++ Full-Stack-Open/Part_1/src/App.jsx
@@ -0,0 +1,15 @@
+const App = () => {
+  const friends = [
+    { name: 'Peter', age: 4 },
+    { name: 'Maya', age: 10 },
+  ]
+
+  return (
+    <div>
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+    </div>
+  )
+}
+
+export default App
