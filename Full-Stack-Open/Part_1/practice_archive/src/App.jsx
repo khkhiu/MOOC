6a0af29d7d9e33ee8097ddb484e5755afe144988
khkhiu <khkhiu@gmail.com>
2024-08-13 21:50:45 +0800

    Learning destructure in JS

--- Full-Stack-Open/Part_1/practice_archive/src/App.jsx
+++ Full-Stack-Open/Part_1/practice_archive/src/App.jsx
@@ -1,9 +1,13 @@
+/*Using destructures */
 const Hello = (props) => {
+  /*Alt props usage: const Hello = ({name, age}) => {} */
+  const {name, age} = props
+  const bornYear = () => new Date().getFullYear() - age
+
   return (
     <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old
-      </p>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
@@ -20,3 +24,33 @@ const App = () => {
     </div>
   )
 }
+
+/* original code for reference
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear()
+    return yearNow - props.age
+  }  
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+*/
