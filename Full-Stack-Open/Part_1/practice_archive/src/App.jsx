33c9e992f23614c8d108abd302e5d577f6a2e57a
khkhiu <khkhiu@gmail.com>
2024-08-17 20:42:08 +0800

    Learning re-rendering during state change

--- Full-Stack-Open/Part_1/practice_archive/src/App.jsx
+++ Full-Stack-Open/Part_1/practice_archive/src/App.jsx
@@ -1,4 +1,41 @@
+//Re-rendering during state change
+import { useState } from "react"
+
+const App = () => {
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value', counter)
+
+  const Display = ({ counter }) => <div>{counter}</div>
+
+  const Button = ({ onSmash, text }) => <button onClick={onSmash}>{text}</button>
+
+  const increaseByOne = () => {
+    console.log('increasing, value before', counter)    
+    setCounter(counter + 1)
+  }
+
+  const decreaseByOne = () => { 
+    console.log('decreasing, value before', counter)    
+    setCounter(counter - 1)
+  }
+
+  const setToZero = () => {
+    console.log('resetting to zero, value before', counter)    
+    setCounter(0)
+  }
+
+  return (
+    <div>
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
+    </div>
+  )
+} 
+
 //Passing state to child components
+/*
 import { useState } from "react"
 
 const App = () => {
@@ -47,6 +84,7 @@ const App = () => {
     </div>
   )
 }
+*/
 
 //Event handler as a functions
 /*
