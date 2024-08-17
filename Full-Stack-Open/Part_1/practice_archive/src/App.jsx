cf2805fcbfc78f6d8efe241110b7de04b15407ce
khkhiu <khkhiu@gmail.com>
2024-08-17 20:36:21 +0800

    Learning passing state to child components

--- Full-Stack-Open/Part_1/practice_archive/src/App.jsx
+++ Full-Stack-Open/Part_1/practice_archive/src/App.jsx
@@ -1,6 +1,56 @@
-//Event handler as a functions
-import { useState } from 'react'
+//Passing state to child components
+import { useState } from "react"
 
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
+  const Display = (props) => {
+    return (
+      <div>{props.counter}</div>
+    )
+  }
+
+  const Button = (props) => {
+    return (
+      <button onClick={props.onClick}>
+        {props.text}
+      </button>
+    )
+}
+
+  const increaseByOne = () => setCounter(counter + 1)    
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+  
+  
+  return (
+    <div>
+      <Display 
+        counter={counter}
+      />     
+      
+      <Button
+        onClick={increaseByOne}
+        text='plus'
+      />
+      
+      <Button 
+        onClick={setToZero} 
+        text="zero"
+      />
+      
+      <Button        
+        onClick={decreaseByOne}        
+        text='minus'      
+      />   
+
+    </div>
+  )
+}
+
+//Event handler as a functions
+/*
+import { useState } from 'react'
 
 const App = () => {
   const [ counter, setCounter ] = useState(0)
@@ -21,7 +71,7 @@ const App = () => {
     </div>
   )
 }
-
+*/
 
 // Fun with state hooks and event handling
 /*
