3c9b3a631cd7c724b071b59139018f22b9aca965
khkhiu <khkhiu@gmail.com>
2024-08-18 12:55:56 +0800

    learning about asynchronous updates

--- Full-Stack-Open/Part_1/practice_archive/src/App.jsx
+++ Full-Stack-Open/Part_1/practice_archive/src/App.jsx
@@ -1,19 +1,28 @@
-//Arrays
+//Arrays and asynchronous update
 import { useState } from "react"
 
 const App = () => {
   const [left, setLeft] = useState(0)
   const [right, setRight] = useState(0)
   const [allClicks, setAll] = useState([])
+  const [total, setTotal] = useState(0)
 
   const handleLeftClick = () => {
     setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+    console.log('left before', left)
+    const updatedLeft = left + 1
+    setLeft(updatedLeft)
+    console.log('left after', updatedLeft)
+    setTotal(updatedLeft + right)
   }
 
   const handleRightClick = () => {
     setAll(allClicks.concat('R'))
-    setRight(right + 1)
+    console.log('right before', right)
+    const updatedRight = right + 1
+    setRight(updatedRight)
+    console.log('right after', updatedRight)
+    setTotal(left + updatedRight)
   }
 
   return (
@@ -23,6 +32,7 @@ const App = () => {
       <button onClick={handleRightClick}>Right</button>
       {right}
       <p>{allClicks.join(' ')}</p>
+      <p>Total {total}</p>
     </div>
   )
 }
