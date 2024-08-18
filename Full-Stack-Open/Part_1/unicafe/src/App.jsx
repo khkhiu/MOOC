df66c889ae164ca74f2f1f1e2adbf585413cf87f
khkhiu <khkhiu@gmail.com>
2024-08-18 16:41:34 +0800

    Completed 'unicafe' exercise
    Completed EX1.6 to EX1.11

--- Full-Stack-Open/Part_1/unicafe/src/App.jsx
+++ Full-Stack-Open/Part_1/unicafe/src/App.jsx
@@ -1,35 +1,145 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+const StatisticLine = ({text, value}) => {
+  return(
+    <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
   )
 }
 
+const Statistics = ({good, neutral, bad, all}) => {
+  if (all === 0){
+    return (
+      <div>
+        <h1>Statistics</h1>
+        No feedback given yet
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>Statistics</h1>
+          <table>
+            <tbody>
+            <StatisticLine text="Good" value={good}/>
+            <StatisticLine text="Neutral" value={neutral}/>
+            <StatisticLine text="Bad" value={bad}/>
+            <StatisticLine text="All" value={all}/>
+            <StatisticLine text="Average" value={(good-bad)/all}/>
+            <StatisticLine text="Good" value={(good/all)*100+'%'}/>
+            </tbody>
+          </table>
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () =>{
+    console.log('Good before', good)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    console.log('Good after', updatedGood)
+    setAll(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () =>{
+    console.log('Neutral before', neutral)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    console.log('Neutral after', updatedNeutral)
+    setAll(good + updatedNeutral + bad)
+  }
+
+  const handleBadClick = () =>{
+    console.log('Bad before', bad)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    console.log('Bad after', updatedBad)
+    setAll(good + neutral + updatedBad)
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGoodClick} text='Good'/>
+      <Button handleClick={handleNeutralClick} text='Neutral'/>
+      <Button handleClick={handleBadClick} text='Bad'/>
+      <Statistics good={good} neutral = {neutral} bad={bad} all={all}/>
+    </div>
+  )
+}
+
+//Reference code
+/*
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () =>{
+    console.log('Good before', good)
+    const updatedGood = good + 1
+    setGood(updatedGood)
+    console.log('Good after', updatedGood)
+    setAll(updatedGood + neutral + bad)
+  }
+
+  const handleNeutralClick = () =>{
+    console.log('Neutral before', neutral)
+    const updatedNeutral = neutral + 1
+    setNeutral(updatedNeutral)
+    console.log('Neutral after', updatedNeutral)
+    setAll(good + updatedNeutral + bad)
+  }
+
+  const handleBadClick = () =>{
+    console.log('Bad before', bad)
+    const updatedBad = bad + 1
+    setBad(updatedBad)
+    console.log('Bad after', updatedBad)
+    setAll(good + neutral + updatedBad)
+  }
+
+  return (
+    <div>
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGoodClick} text='Good'/>
+      <Button handleClick={handleNeutralClick} text='Neutral'/>
+      <Button handleClick={handleBadClick} text='Bad'/>
+      <div>
+        <h1>Statistics</h1>
+        <p>Good: {good}</p>
+        <p>Neutral: {neutral}</p>
+        <p>Bad: {bad}</p>
+        <p>All: {all}</p>
+        <p>Average: {(good-bad)/all}</p>
+        <p>Positive: {good/all*100} %</p>
+      </div>
+    </div>
+  )
+}
+*/
 export default App
--- Full-Stack-Open/Part_1/unicafe/src/main.jsx
+++ Full-Stack-Open/Part_1/unicafe/src/main.jsx
@@ -1,10 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
