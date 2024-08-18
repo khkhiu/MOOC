f78a195ca4f23d7ac27530fe8cd7a8b67ca25432
khkhiu <khkhiu@gmail.com>
2024-08-18 12:34:36 +0800

    Learning arrays

--- Full-Stack-Open/Part_1/practice_archive/src/App.jsx
+++ Full-Stack-Open/Part_1/practice_archive/src/App.jsx
@@ -1,5 +1,35 @@
-// Complex state
+//Arrays
+import { useState } from "react"
 
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  return (
+    <div>
+      {left}
+      <button onClick={handleLeftClick}>Left</button>
+      <button onClick={handleRightClick}>Right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+    </div>
+  )
+}
+
+
+// Complex state
+/*
 import { useState } from "react"
 
 const App = () => {
@@ -32,7 +62,7 @@ const App = () => {
     </div>
   )
 }
-
+*/
 
 //Re-rendering during state change
 /*
