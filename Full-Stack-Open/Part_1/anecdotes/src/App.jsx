2aaf4070d683304c7b9f13d436b55630e57a02d2
khkhiu <khkhiu@gmail.com>
2024-08-18 18:21:27 +0800

    Completed 'Anecdote' excercise
    Complete EX1.12 to EX1.14

--- Full-Stack-Open/Part_1/anecdotes/src/App.jsx
+++ Full-Stack-Open/Part_1/anecdotes/src/App.jsx
@@ -1,34 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Button = ({handleClick, text}) => (
+  <button onClick = {handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+   
+  const [selected, setSelected] = useState(0)
+  //Use anecdotes.length to specify Array size, and start the score array with all 0
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0))
+
+  const setAnecdote = () => {
+    // Use math.floor to constraint RNG to length of anecdote array
+    setSelected(Math.floor(Math.random() * anecdotes.length))
+    console.log("Anecdote number: " + selected)
+  }
+
+  const addVote = () => {
+    // Component to track number of votes, see "Spread syntax" for reference
+    const anecdotePoints = [...points]
+    // Associate anecdote with votes
+    anecdotePoints[selected] += 1
+    setPoints(anecdotePoints)
+    console.log("Anecdote number: "+selected+" Anecdote vote: "+anecdotePoints)
+  }
+
+  // Component to track which anecdote has the most number of points
+  const bestAnecdote = points.indexOf(Math.max(...points))
 
   return (
-    <>
+    <div>
+      <h1>Anecdote of the day</h1><br/>
+      <div>{anecdotes[selected]}</div><br/>
+      <div>This anecdote has {points[selected]} votes</div><br/>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button handleClick={()=>{setAnecdote()}} text='Next anecdote'/>
+        <Button handleClick={()=>{addVote()}} text='Vote'/>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <h1>Anecdote with most votes</h1>
+      <div>{anecdotes[bestAnecdote]}</div><br/>
+      <div>This anecdote has {points[bestAnecdote]} votes</div>
+    </div>
   )
 }
 
