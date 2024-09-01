421588239aad69f4fe0596e0e3cb49b3cfa3ebd9
khkhiu <khkhiu@gmail.com>
2024-09-01 21:38:45 +0800

    Complete EX2.2 and EX2.3

--- Full-Stack-Open/Part_2/CourseInformation-2/src/App.jsx
+++ Full-Stack-Open/Part_2/CourseInformation-2/src/App.jsx
@@ -16,13 +16,20 @@ const Header = ({ name }) => {
 };
 
 // Content component takes a list of parts as a prop and renders each part
+// Also calculate the total number of exercises
 const Content = ({ parts }) => {
+
+  // Calculate the total number of exercises using reduce
+  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
+
   return (
     <div>
       {parts.map(part => (
         <Part key={part.id} part={part} />
         /* Render each part using Part component*/
       ))}
+      <p><strong>Total exercises: {totalExercises}</strong></p>  
+      {/* Display the total number of exercises*/}
     </div>
   );
 };
@@ -58,7 +65,12 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      }
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+      }      
     ]
   }
 
